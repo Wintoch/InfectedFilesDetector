114b0f6d1ce44ea8178f908d3640342575f4d549
Wintoch <kub123-2001@wp.pl>
2026-07-01 00:21:13 +0200

    few minor changes to not loose track of the project

--- UI.py
+++ UI.py
@@ -1,10 +1,24 @@
 import customtkinter as ctk
 from tkinter import filedialog
+import pefile
 
 ctk.set_appearance_mode("dark")
 
+scanPath = ""
+
+def extractFeatures():
+    pass
+
+def loadFile():
+    global scanPath
+    scanPath = filedialog.askopenfilename()
+
 def scanFile():
-    print("Button works")
+    global scanPath
+    if (scanPath == ""):
+        labelReturn.configure(text="Error, you need to load the file")
+    else:
+        pe = pefile.PE(scanPath)
 
 window = ctk.CTk()
 window.title("Infected Files Detector")
@@ -13,7 +27,13 @@ window.geometry("500x400")
 label = ctk.CTkLabel(window, text="When ready, click button below")
 label.pack(pady=20)
 
-button = ctk.CTkButton(window, text="Choose file and scan", command=scanFile, corner_radius=20)
+button = ctk.CTkButton(window, text="Load file", command=loadFile, corner_radius=20)
 button.pack(pady=10)
+
+button = ctk.CTkButton(window, text="Scan", command=scanFile, corner_radius=20)
+button.pack(pady=10)
+
+labelReturn = ctk.CTkLabel(window, text="")
+labelReturn.pack(pady=20)
 
 window.mainloop()
